bacd15b50b58eff8b38a7ca0a43d51832964baf6
grace.frank <grace.frank@interswitchgroup.com>
2019-11-28 11:39:41 +0100

    updated the user details to include a real userId

--- src/main/java/com/user/usermanagement/service/implementation/UserServiceImpl.java
+++ src/main/java/com/user/usermanagement/service/implementation/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.user.usermanagement.service.implementation;
 import com.user.usermanagement.UserRepository;
 import com.user.usermanagement.io.entity.UserEntity;
 import com.user.usermanagement.service.UserService;
+import com.user.usermanagement.shared.Utils;
 import com.user.usermanagement.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +14,20 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
-    @Override
 
+    @Autowired
+    Utils utils;
+
+    @Override
    public  UserDto createUser (UserDto user){
+        UserEntity existingUser = userRepository.findByEmail(user.getEmail());
+        if(existingUser != null) throw new  RuntimeException("Record already exist");
+
         UserEntity userEntity = new UserEntity();
 
         BeanUtils.copyProperties(user, userEntity);
 
-        userEntity.setUserId("testUserId");
+        userEntity.setUserId(utils.generatedUserId(30)  );
         userEntity.setEncryptedPassword("test");
 
         UserEntity storedUserDetails =  userRepository.save(userEntity);
