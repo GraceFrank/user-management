49e315a3fd119a5b10a4eaac1cc2279de060c770
grace.frank <grace.frank@interswitchgroup.com>
2019-11-28 11:38:59 +0100

    added a utility for generationg random public userid

--- src/main/java/com/user/usermanagement/shared/Utils.java
+++ src/main/java/com/user/usermanagement/shared/Utils.java
@@ -1,4 +1,27 @@
-package com.user.usermanagement.shared.dto;
+package com.user.usermanagement.shared;
 
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+@Component
 public class Utils {
+    private final Random RANDOM = new SecureRandom();
+    private final String ALPHABET = "1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+    private final int  ITERATIONS = 10000;
+    private final int KEY_LENGTH = 256;
+
+    public String generatedUserId(int length){
+        return generateRandomString(length);
+    }
+
+    private String generateRandomString(int length) {
+        StringBuilder returnValue = new StringBuilder(length);
+
+        for(int i =0; i < length; i++){
+            returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+        return new String(returnValue);
+    }
 }
